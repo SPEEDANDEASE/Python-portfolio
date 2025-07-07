41da59b5b4f416162156976dabd1e9a8f79f73df
Caleb Okoh <okohcaleb8@gmail.com>
2025-07-08 00:36:23 +0100

    Expense tracker.py

---
+++ Expense tracker.py
@@ -0,0 +1,24 @@
+#Expense Tracker (CLI App)
+
+A simple terminal-based Python application to track your daily expenses by category.
+
+# Features
+- Add expenses under categories (Food, Transport, Accommodation, Data)
+- Automatically fills today's date or lets you enter a custom one
+- View today's expenses
+- Print full summary of all transactions
+- Uses functions, lists, dictionaries, and modules
+
+# Skills Demonstrated
+- Python functions & modular design
+- Data structures: lists, dictionaries
+- Date handling with `datetime`
+- CLI design and input validation
+
+# How to Run
+
+1. Clone or download this project.
+2. Open terminal or VS Code in the folder.
+3. Run this command:
+   ```bash
+   python report.py
